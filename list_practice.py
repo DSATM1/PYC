87e4bd2058ea804bd91d194fc5d12a135875af69
Suraj <surajsppokale@gmail.com>
2026-06-08 13:09:45 +0530

    Comment out "Hello World!" and add a block of code for user input and name enumeration

--- list_practice.py
+++ list_practice.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+#print("Hello World!")
+
+"""n = int(input("Enter size: "))
+nums = []
+for i in range(n):
+    value = input("Enter Names: ")
+    nums.append(value)
+print(nums)
+for i, name in enumerate(nums):
+     print(f"{i+1}. {name}")"""
