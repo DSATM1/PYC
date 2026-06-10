84b2a87d0e6161d97e891be91035f41c56ad99da
Suraj <surajsppokale@gmail.com>
2026-06-10 17:50:58 +0530

    Refactor reverse_list function and clean up example usage

--- list_pr_2.py
+++ list_pr_2.py
@@ -3,15 +3,7 @@
 """reverse_list([1, 2, 3, 4, 5]) → [5, 4, 3, 2, 1]
    reverse_list(["a", "b", "c"]) → ["c", "b", "a"]"""
 
-
-"""reverse_list = []
-i = len(lst) - 1
-while i >= 0:
-   reverse_list += lst[i]
-   i = i - 1
-return reverse_list"""
-
-def reverse_list(lst): #type:ignore
+"""def reverse_list(lst): #type:ignore
    res = []
    i = len(lst) - 1  #type:ignore
    while i>= 0:
@@ -19,4 +11,22 @@ def reverse_list(lst): #type:ignore
       i = i - 1
    return res #type:ignore
 print(reverse_list([1, 2, 3, 4, 5])) #type:ignore
-print(reverse_list(["a","b","c","d","e"])) #type:ignore
+print(reverse_list(["a","b","c","d","e"])) #type:ignore"""
+
+
+"""lst = [1, 2, 3, 4, 5]
+
+reverse_list = []
+
+i = len(lst) - 1
+
+while i >= 0:
+    reverse_list.append(lst[i])
+    i = i - 1
+
+print(reverse_list)"""
+
+def palindrome(pal):
+    return pal == pal[::-1]
+res = palindrome("madaM")
+print(res)
